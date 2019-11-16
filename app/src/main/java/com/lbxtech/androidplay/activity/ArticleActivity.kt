39bb70515effcf99fa1f2fc4d7d85ae6fd809fa3
WangHao <wh@lbx.dev>
2019-11-16 15:48:33 +0800

    webview go back

--- app/src/main/java/com/lbxtech/androidplay/activity/ArticleActivity.kt
+++ app/src/main/java/com/lbxtech/androidplay/activity/ArticleActivity.kt
@@ -1,7 +1,9 @@
 package com.lbxtech.androidplay.activity
 
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.FrameLayout
 import com.lbxtech.androidplay.R
 import com.lbxtech.androidplay.base.BindView
@@ -19,9 +21,23 @@ class ArticleActivity : BaseActivity() {
         val link = intent.getStringExtra("link")
         webView = WebView(this).apply {
             layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+                    view.loadUrl(request.url.toString())
+                    return true
+                }
+            }
         }
         mainFrame?.addView(webView)
         webView?.loadUrl(link)
+    }
+
+    override fun onBackPressed() {
+        if (webView?.canGoBack() == true) {
+            webView?.goBack()
+            return
+        }
+        super.onBackPressed()
     }
 
     override fun onDestroy() {
